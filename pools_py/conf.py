a3944b59e716ffad8695e515b67827c6c2284968
debugtitan <alts.devs@gmail.com>
2024-05-04 18:55:31 +0100

    configuration file updated

--- pools_py/conf.py
+++ pools_py/conf.py
@@ -1,2 +1,20 @@
+import os
+import logging
 from dotenv import dotenv_values
-from pathlib
+from pathlib import Path
+
+logging.basicConfig(
+    format="%(asctime)s → %(levelname)s → %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+env_path = os.path.join(BASE_DIR, ".env")
+config = None
+
+if os.path.exists(env_path):
+    config = dotenv_values(env_path)
+else:
+    logger.error('create .env file')
+    exit()
+
