d1db949cb6e1ad7ab0900078fba5f954dba2d9dc
debugtitan <alts.devs@gmail.com>
2024-05-04 17:47:21 +0100

    Python Pool app

--- pools_py/app.py
+++ pools_py/app.py
@@ -5,8 +5,19 @@ from solana.rpc.async_api import AsyncClient
 
 class RaydiumNewPools:
     """
-    `RaydiumNewPools` default app
+    `RaydiumNewPools` events listener
     
     """
+    BASE_RPC_ENDPOINT= ""
+
     def __init__(self):
-        self.client = ""#js
+        self.client = self.setup_client()
+        self.private_client = self.setup_private_client()
+
+    async def setup_client(self):
+        """ setup client connection"""
+        return AsyncClient(self.BASE_RPC_ENDPOINT)
+
+
+    async def setup_private_client(self):
+        """ setup a private rpc client endpoint"""
