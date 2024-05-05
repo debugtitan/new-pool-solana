31f80274a3f47443d65c213971db379751f8ab8e
debugtitan <alts.devs@gmail.com>
2024-05-05 08:25:13 +0100

    listen to newly added liqudity

--- pools_py/app.py
+++ pools_py/app.py
@@ -1,10 +1,10 @@
 import asyncio
 from unittest import result
-from solders.pubkey import Pubkey # type: ignore
+from solders.pubkey import Pubkey  # type: ignore
 from solana.rpc.async_api import AsyncClient
 from asyncstdlib import enumerate
 from solana.rpc.websocket_api import connect
-from solders.rpc.config import RpcTransactionLogsFilterMentions # type: ignore
+from solders.rpc.config import RpcTransactionLogsFilterMentions  # type: ignore
 from conf import config, logger
 
 
@@ -15,7 +15,9 @@ class RaydiumNewPools:
     """
 
     BASE_WSS_ENDPOINT = "wss://api.mainnet-beta.solana.com"
-    RAYDIUM_AUTHORITY = Pubkey.from_string("5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1")
+    RAYDIUM_AUTHORITY = Pubkey.from_string(
+        "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1"
+    )
     ENV_ATTR = ["PRIVATE_CLIENT", "TELEGRAM_CHANNEL", "TELEGRAM_BOT_TOKEN"]
 
     def __init__(self):
@@ -28,15 +30,18 @@ class RaydiumNewPools:
 
     async def subscribe_to_log(self):
         async with connect(self.BASE_WSS_ENDPOINT) as websocket:
-            await websocket.logs_subscribe(RpcTransactionLogsFilterMentions(self.RAYDIUM_AUTHORITY),commitment="finalized")
+            await websocket.logs_subscribe(
+                RpcTransactionLogsFilterMentions(self.RAYDIUM_AUTHORITY),
+                commitment="finalized",
+            )
             logger.info("STARTED EVENT!!")
             while True:
                 data = await websocket.recv()
                 _result = data[0].result
-                if hasattr(_result,'value'):
+                if hasattr(_result, "value"):
                     result = _result.value
                     log_signature, logs = result.signature, result.logs
-                    if any("Program log: Error: The amm account owner is not match with this program" in log for log in logs):
+                    if any("Program log: initialize2:" in log for log in logs):
                         print(log_signature)
                 else:
                     logger.warning(_result)
@@ -51,8 +56,6 @@ class RaydiumNewPools:
         # setup our private node
         await self.setup_private_client()
         await self.subscribe_to_log()
-        
-
 
 
 if __name__ == "__main__":
